02fecc2e8c303df2b023e2c4c9420ff54e9be73e
Dwaynekj <Dwaynekj@gmail.om>
2013-07-12 15:36:09 -0400

    Initial Commit

---
+++ LeapGame/game.js
@@ -0,0 +1,24 @@
+// Leap.loop({enableGestures: true}, function(frame, done) {
+  
+//   var frameOutput = document.getElementById("handData");
+//   var handString = '';
+//   done()
+//   if (frame.gestures.length !== 0) {
+//     handString = "I just tapped the Screen!!"
+//     console.dir(frame)
+  
+//   // if (frame.hands.length > 0) {
+//   // 	for (var i =0; i < frame.hands.length; i++){
+//   // 	 	hand = frame.hand[i]
+//   // 	}
+
+//   // 	handString = frame.hands.length + "";
+
+
+//   } else {
+//   	handString = "Did I just Screen Tapped";
+//   }
+//     frameOutput.innerHTML = handString;
+// })
+
+
